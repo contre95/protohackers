e388637d597c9b3e8cd1cb12002b413e654010eb
contre95 <lucascontre95@gmail.com>
2023-01-21 00:06:27 +0100

    0_smoke_test

--- 0_smoke_test/main.go
+++ 0_smoke_test/main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
-	"sync/atomic"
 )
 
 func main() {
-	err := StartTCPServer(8584, "0.0.0.0", 150)
+	err := StartTCPServer(8584, "0.0.0.0", 10)
 	if err != nil {
 		fmt.Printf("Could not start server: %v", err)
 	}
-
 }
 
 func StartTCPServer(port int, host string, maxPoolConnection int) error {
@@ -28,14 +27,33 @@ func StartTCPServer(port int, host string, maxPoolConnection int) error {
 		if conns <= uint64(maxPoolConnection-1) {
 			// Holds inil a new connection is set
 			conn, err := ln.Accept()
-			atomic.AddUint64(&conns, 1)
+			//atomic.AddUint64(&conns, 1)
 			if err != nil {
 				log.Println("Error creating connection")
 				panic(err)
 			}
-			go tcpHandler(&conn)
+			go tcpHandler(conn)
 		}
 	}
 }
 
-func tcpHandler(conn *net.Conn) {}
+func tcpHandler(conn net.Conn) {
+	defer conn.Close()
+	for {
+		buf := make([]byte, 512)
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Reached EOF, closing connection")
+				break
+			}
+			fmt.Println(err)
+			break
+		}
+		_, err = conn.Write(buf[:n])
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+}
